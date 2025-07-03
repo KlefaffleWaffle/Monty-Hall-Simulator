fd83b1721c846e989ab4bcdbe6ab90dd875aa24f
KlefaffleWaffle <austinsjobemail27@gmail.com>
2025-07-02 22:56:30 -0700

    Add files via upload

---
+++ Monty_Hall_Simulation.py
@@ -0,0 +1,62 @@
+
+import random
+
+#This program was made for demonstration purposes on my YouTube channel. Some code may be unoptimized, but I feel it helps non-coders understand the logic.
+#In regards to my portfolio, it is being used to show that I have familiarity with python, but is not meant to showcase advanced logic or mathematical skills.
+
+# wins
+wins = 0
+# games
+games = 0
+# Run 10,000 times.
+
+while games < 10000:
+    # Make a list of 1-3 (Not necessary, but useful for demonstration purposes)
+    doors = [1, 2, 3]
+    removableDoors = doors.copy();
+    
+    # Pick a value between 1 and 3. This is the winning door
+    winningDoor = random.randint(1, 3)
+
+    # Simulate user picking a door by selecting a random value of 1-3
+    userDoor = random.randint(1, 3)
+
+    # Remove door that is neither the contestant's door NOR the winning door. (Necessary due to line 1, but not normally)
+    
+    try:
+        #exclude winning door
+        removableDoors.remove(winningDoor)
+        #exclude the user's choice
+        removableDoors.remove(userDoor)
+    except ValueError:
+        pass  # Value not found, no problem
+
+        #At this point, we will have one or two options, pick a random one.
+    removedDoor = random.choice(removableDoors)
+
+
+    try:
+        #actually remove the door
+        doors.remove(removedDoor)
+    except ValueError:
+        pass  # Value not found, no problem
+
+
+    # contestant switches value.
+        #To future proof program for derived projects, we did this as a random and an error checker.
+    userDoorOld = userDoor
+    userDoor = random.choice(doors)
+    while userDoor == userDoorOld:
+        userDoor = random.choice(doors)
+    
+
+    # if switched value == winning door, wins ++;
+    if userDoor == winningDoor:
+        wins+=1
+    # games++;
+    games += 1
+
+# print wins, / games."%",
+        #Note to self: need to convert to strings before displaying
+print ("Games won: " + str(wins) + "\nTotal Games: " + str(games) + "\n" + str(wins/games *100) +"%")
+
